7d6db8191913e02f0e2eba8f0e67782bb78a546f
Craig Treptow <craig.treptow@gmail.com>
2018-12-02 14:38:55 -0600

    rejiggering while going through the 'mastering the shell' upcase trail

---
+++ zsh/aliases.zsh
@@ -0,0 +1,64 @@
+################################################################################
+# general overrides
+################################################################################
+# Use modern regexp, i.e. "(one|two)", not "(\(one\|two\)"
+alias sed="sed -E"
+alias cp="cp -iv"
+alias rm="rm -iv"
+alias mv="mv -iv"
+alias ls="ls -FGh"
+alias du="du -cksh"
+alias df="df -h"
+
+################################################################################
+# going places
+################################################################################
+alias code="cd ~/code"
+alias gods"cd ~/code/dotfiles"
+alias goe="cd ~/code/exercism/elixir"
+alias study="cd ~/code/study-notes"
+alias work="~/code/work/dotfiles/work"
+alias gow="cd ~/code/work"
+
+################################################################################
+# process related
+################################################################################
+alias kkr="pkill -9 -f 'rb-fsevent|rails|spring|puma|webpack|ruby'"
+alias kkz="pkill -9 'zsh'"
+alias uar='cd $MBC_WORK_ROOT/mbc_tools; bundle exec $MBC_WORK_ROOT/mbc_tools/scripts/update_all_repos'
+
+################################################################################
+# postgresql related
+################################################################################
+
+alias postgresstart='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
+alias postgresstop='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
+
+################################################################################
+# Git related
+################################################################################
+alias gl='git log --oneline --decorate --graph --stat'
+alias gitcleanup='git branch --merged | egrep -v "(^\*|master|develop)" | xargs -n 1 git branch -d'
+
+################################################################################
+# Rubocop related
+################################################################################
+# Before commit
+alias rbc='clear;git ls-files -m | xargs ls -1 2>/dev/null | grep '\.rb$' | xargs rubocop'
+# Before push
+alias rbp='clear;git diff-tree -r --no-commit-id --name-only master@\{u\} head | xargs ls -1 2>/dev/null | grep '\.rb$' | grep -v schema | xargs rubocop'
+
+alias rac='rubocop --auto-correct'
+
+################################################################################
+# tmux related
+################################################################################
+alias tml="tmux list-sessions"       #list running tmux sessions
+alias tma="tmux -2 attach -t $1"     #join a current session
+alias tmk="tmux kill-session -t $1"  #kill a session
+alias mux="tmuxinator"
+
+################################################################################
+# brew related
+################################################################################
+alias cleanup="brew update; brew upgrade; brew cleanup"
---
+++ zsh/ruby.zsh
@@ -0,0 +1,15 @@
+export COV=1
+export DISABLE_DATABASE_ENVIRONMENT_CHECK=1    # set so rake will run all the tests after Rails 5
+export EDITOR="vim"
+export MBC_HOST=http://localhost:3000
+export MBC_WORK_ROOT=$HOME/code/work
+export PATH="/usr/local/heroku/bin:$PATH"
+export REDIRECT_SIGN_IN_TO_DOMINO=false
+export RUBY_GC_MALLOC_LIMIT=100000000
+export RUBY_FREE_MIN=500000
+export RUBY_HEAP_MIN_SLOTS=1000000
+export RUBY_GC_HEAP_FREE_SLOTS=500000
+export RUBY_GC_HEAP_INIT_SLOTS=1000000
+export RUBY_HEAP_SLOTS_INCREMENT=1000000
+export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
+ulimit -n 4096
